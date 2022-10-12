6e0a3f35d9a20f2cc062ebe7da09e0d4eda02d1b
Jonathan Tay <jontay999@gmail.com>
2022-10-12 13:38:20 -0700

    Levenshtein space optimized

--- Levenshtein Edit Distance/solve.py
+++ Levenshtein Edit Distance/solve.py
@@ -22,3 +22,19 @@ def edit_distance(x, y):
             else:
                 matrix[i][j] = min(matrix[i-1][j], matrix[i-1][j-1], matrix[i][j-1])+1
     return matrix[-1][-1]
+
+
+# Using 2 rows only
+def edit_distance_space_optimized(x, y):
+    space_matrix = [[i for i in range(len(y)+1)], [1] + [0 for _ in range(len(y))]]
+    
+    for i in range(1, len(x)+1):
+        row = i%2
+        space_matrix[row][0] = i
+        for j in range(1, len(y)+1):
+            if x[i-1] == y[j-1]:
+                space_matrix[row][j] = space_matrix[(row-1)%2][j-1]
+            else:
+                space_matrix[row][j] = min(space_matrix[(row-1)%2][j], space_matrix[(row-1)%2][j-1], space_matrix[row][j-1])+1
+    final_row = len(x)%2
+    return space_matrix[final_row][-1]
