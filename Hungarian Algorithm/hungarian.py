c94c10a21b92a0dbdf4cc14a7df5f144cff968f8
Jonathan Tay <jontay999@gmail.com>
2022-11-05 00:45:35 -0700

    a few more comments for hungarian

--- Hungarian Algorithm/hungarian.py
+++ Hungarian Algorithm/hungarian.py
@@ -25,6 +25,7 @@ def hungarianMatch(matrix, minimize=True):
 
         p[0] = i 
 
+        # minv is a labelling of a col nodes
         minv = [float('inf') for _ in range(cols+1)]
         used = [False for _ in range(cols+1)]
 
@@ -38,11 +39,18 @@ def hungarianMatch(matrix, minimize=True):
             j1 = None
 
             for j in range(1,cols+1):
+
+                # If col[j] has not been paired jp
                 if not used[j]:
+                    # do a  labelling, edge weight - label of row - label of col
                     cur = matrix[i0-1][j-1] - u[i0] - v[j]
+                    
+                    # minv is the curr labelling of the col[v] coming from row[i], update the labelling of minv if it is better
                     if cur < minv[j]:
                         minv[j] = cur 
                         way[j] = j0
+                    
+                    # im guessing that delta is the slack that you can push through the row[i] node and is limited by min[v]
                     if minv[j] < delta:
                         delta = minv[j]
                         j1 = j
@@ -65,6 +73,18 @@ def hungarianMatch(matrix, minimize=True):
             if not j0:
                 break
     
+
+    # // For each N, it contains the M it selected
+    # vector<ll> ans(n+1);
+    # for(ll j = 1; j <= m ; ++j)
+    #     ans[p[j]] = j;
+
+    
     if not minimize: return v[0]
     return -v[0]
+
+
+arr = [
+    []
+]
 
